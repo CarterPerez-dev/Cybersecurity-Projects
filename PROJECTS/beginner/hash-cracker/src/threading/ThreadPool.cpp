3e2c3ff002b0a1c806092906a7db2e296800bb10
CarterPerez-dev <cartertechsolution@gmail.com>
2026-03-22 10:29:06 -0400

    feat: Engine, ThreadPool, and Progress display
    ThreadPool: jthread-based work partitioning with SharedState atomics.
    Progress: rich terminal output with Unicode box drawing, ANSI colors,
    progress bar, speed/ETA display, and cracked/exhausted result rendering.
    Engine: template function wiring hasher + attack + threading + salt.
    3 integration tests passing (crack, exhaust, salted crack).

--- PROJECTS/beginner/hash-cracker/src/threading/ThreadPool.cpp
+++ PROJECTS/beginner/hash-cracker/src/threading/ThreadPool.cpp
@@ -15,6 +15,15 @@ ThreadPool::ThreadPool(unsigned thread_count)
     : thread_count_(thread_count > 0 ? thread_count
                                      : std::thread::hardware_concurrency()) {}
 
-void ThreadPool::run(WorkFn) {}
+void ThreadPool::run(WorkFn work) {
+    std::vector<std::jthread> threads;
+    threads.reserve(thread_count_);
+
+    for (unsigned i = 0; i < thread_count_; ++i) {
+        threads.emplace_back([this, &work, i] {
+            work(i, thread_count_, state_);
+        });
+    }
+}
 
 SharedState& ThreadPool::state() { return state_; }
